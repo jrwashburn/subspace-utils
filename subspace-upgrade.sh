f34c2ce6b3b883561ce11abd08cde68026e5ff13
Jonathan Washburn <jrwashburn@gmail.com>
2023-10-12 19:19:56 -0400

    fix regression for architecture

--- subspace-upgrade.sh
+++ subspace-upgrade.sh
@@ -56,10 +56,10 @@ systemctl --user stop subspace-node
 echo Downloading latest build
 
 LAST_BUILD_MONTH_DAY=$(curl https://api.github.com/repos/subspace/subspace/releases | grep name | grep \"gemini- | cut -d : -f2 | cut -d - -f4,5 | cut -d \" -f1 | sort -M | tail -n1)
-LATEST_NODE=$(curl https://api.github.com/repos/subspace/subspace/releases | grep $(date +%Y-$LAST_BUILD_MONTH_DAY) | grep \"gemini- | grep browser_download_url | grep $PLATFORM-v$CPULEVEL |  grep ubuntu | grep node | cut -d : -f2,3 |  tr -d ' "')
-LATEST_FARMER=$(curl https://api.github.com/repos/subspace/subspace/releases | grep $(date +%Y-$LAST_BUILD_MONTH_DAY) | grep \"gemini- | grep browser_download_url | grep $PLATFORM-v$CPULEVEL | grep ubuntu | grep farmer | cut -d : -f2,3 |  tr -d ' "')
+LATEST_NODE=$(curl https://api.github.com/repos/subspace/subspace/releases | grep $(date +%Y-$LAST_BUILD_MONTH_DAY) | grep \"gemini- | grep browser_download_url | grep $PLATFORM-$CPULEVEL | grep node | cut -d : -f2,3 |  tr -d ' "')
+LATEST_FARMER=$(curl https://api.github.com/repos/subspace/subspace/releases | grep $(date +%Y-$LAST_BUILD_MONTH_DAY) | grep \"gemini- | grep browser_download_url | grep $PLATFORM-$CPULEVEL  | grep farmer | cut -d : -f2,3 |  tr -d ' "')
 if [[ "${LATEST_NODE}" = "" || ${LATEST_FARMER} = "" ]] ; then
-    echo Cannot find latest Subspace builds; perhaps due to year rollover and no builds yet this year?
+    echo Cannot find latest Subspace builds - perhaps due to year rollover and no builds yet this year?
     exit
 else
     sudo wget -N $LATEST_NODE -P /opt/subspace/
